9e1af575324dbfae5d1ddabde8425ba875f61b1c
Nicole Marty <nmarty310@gmail.com>
2020-03-18 13:28:27 -0500

    adding images

--- javascript.js
+++ javascript.js
@@ -2,25 +2,4 @@ $(document).ready(function () {
   $('.sidenav').sidenav()
   $('.collapsible').collapsible()
   $('.button-collapse').sideNav()
-  $('.collapsible').collapsible()
-  $('.fixed-action-btn').floatingActionButton()
-  $('.tooltipped').tooltip()
-})
-
-var header = $('header')
-var range = 200
-
-$(window).on('scroll', function () {
-  var scrollTop = $(this).scrollTop(),
-    height = header.outerHeight(),
-    offset = height / 1.1,
-    calc = 1 - (scrollTop - offset + range) / range
-
-  header.css({ opacity: calc })
-
-  if (calc > '1') {
-    header.css({ opacity: 1 })
-  } else if (calc < '0') {
-    header.css({ opacity: 0.6 })
-  }
 })
