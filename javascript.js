264f13438b88a59df1fc6ed31a6e460d3a70553f
Nicole Marty <nmarty310@gmail.com>
2020-03-24 13:47:08 -0500

    shop page layout function working with push pins

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+$('.pushpin-demo-nav').each(function () {
+  var $this = $(this)
+  var $target = $('#' + $(this).attr('data-target'))
+  $this.pushpin({
+    top: $target.offset().top,
+    bottom: $target.offset().top + $target.outerHeight() - $this.height()
+  })
+})
+$(document).ready(function () {
+  $('.pushpin').pushpin()
+})
 $(document).ready(function () {
   $('.sidenav').sidenav()
   $('.collapsible').collapsible()
