bde00042ac6fe8d825d9d8a103f8935d9a77773e
Nicole Marty <nmarty310@gmail.com>
2020-03-25 15:44:01 -0500

    working on finding a good photo for the tops section

--- javascript.js
+++ javascript.js
@@ -1,14 +1,3 @@
-$('.pushpin-demo-nav').each(function () {
-  var $this = $(this)
-  var $target = $('#' + $(this).attr('data-target'))
-  $this.pushpin({
-    top: $target.offset().top,
-    bottom: $target.offset().top + $target.outerHeight() - $this.height()
-  })
-})
-$(document).ready(function () {
-  $('.pushpin').pushpin()
-})
 $(document).ready(function () {
   $('.sidenav').sidenav()
   $('.collapsible').collapsible()
